8fb7a58ee5b0aebbe8d5fc5f4a7d7d7fa990110c
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-12-02 16:03:51 +0200

    Add functionality to handle user authentication errors
    - Implement error handling for authentication failures
    - Update user feedback messages for clarity
    - Refine login and registration process with better validation

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 return [
     'default' => env("DB_DRIVER", 'mysql'),
     'localhost' => env("DB_HOST", 'localhost'),
-    'database' => env("DB_DATABASE", 'mvc_project'),
-    'username' => env('DB_USERNAME', 'root'),
-    'password' => env('DB_PASSWORD', ''),
+    'database' => env("DB_DATABASE", 'u679933726_aloe_vera'),
+    'username' => env('DB_USERNAME', 'u679933726_Admin1'),
+    'password' => env('DB_PASSWORD', '+4Zg3o^]M4-AloeVera'),
 ];
