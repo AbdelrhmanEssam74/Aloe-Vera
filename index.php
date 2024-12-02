7277558e74435b6b79b7c74ce7292efd0ceed89e
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-12-03 00:52:08 +0200

    Fix path for including helpers in the script

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 use Dotenv\Dotenv;
 
-require_once '../src/support/helpers.php'; // Include helpers with base_path()
+require_once 'src/support/helpers.php'; // Include helpers with base_path()
 require_once base_path() . 'vendor/autoload.php'; // Autoloader
 require_once base_path() . 'routes/web.php';     // Routes
 
